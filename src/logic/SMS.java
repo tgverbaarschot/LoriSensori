2736c6d63f99c4886f4a61427940fbb053c4fd6b
FalcoVerhagen88 <42786176+FalcoVerhagen88@users.noreply.github.com>
2018-10-17 21:52:55 +0200

    Update SMS.java
    removed sensitive data

--- src/logic/SMS.java
+++ src/logic/SMS.java
@@ -7,14 +7,14 @@ import com.twilio.type.PhoneNumber;
 public class SMS extends Bericht 
 {
 	
-	public static final String ACCOUNT_SID = "ACa4225ffac03833264f310d7a3b06b370";
-	public static final String AUTH_TOKEN = "d98797ee36683f4c4a4c51a902e11b3c";
-	public static final String PHONE_NUMBER = "+3197014200430";
+	public static final String ACCOUNT_SID = "";
+	public static final String AUTH_TOKEN = "";
+	public static final String PHONE_NUMBER = "+";
 	
 	public static void main(String[]args) 
 	{
 	    SMS sms = new SMS();
-	    String[] telefoonNummers = {"+31627858883", "+31645520760", "+31685861422", "+31652276863" };
+	    String[] telefoonNummers = {""};
 	    EBerichtType berichtType = EBerichtType.ONGEWENSTE_NIVEAU_DALING;
 	    
 	    sms.stuurBericht(telefoonNummers, berichtType);
